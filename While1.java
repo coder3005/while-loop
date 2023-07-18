1ec7ff3381c82d072fa6815aa3ccf8e7ffd0d259
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-18 16:33:36 +0530

    Create While1.java

---
+++ While1.java
@@ -0,0 +1,14 @@
+/* While Loop Example...! */
+import java.util.Scanner;
+class While
+{
+    public static void main(String[] args) {
+
+        int n=1;
+        while(n<=10)
+        {
+            System.out.println("Learn Coding");
+            ++n;
+        }
+    }
+}
