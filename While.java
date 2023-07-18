32dc2db5e76836fcb1d4551c2b89231f78adea8f
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-18 16:29:42 +0530

    Create While.java

---
+++ While.java
@@ -0,0 +1,17 @@
+/* While Loop Example...! */
+import java.util.Scanner;
+class While
+{
+    public static void main(String[] args) {
+
+        int n;
+        System.out.println("Enter value for Condition");
+        Scanner ref=new Scanner(System.in);
+        n=ref.nextInt();
+
+        while(n>=0)
+        {
+            System.out.print("Learn Coding");
+        }
+    }
+}
